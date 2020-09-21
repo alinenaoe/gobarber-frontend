3f1fcc6e81efe6d9243da1eeed1367606f8f0ddc
Aline Naoe <alinenaoe@gmail.com>
2020-09-21 08:50:16 -0300

    Sign In form validation

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import logo from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import { Container, Content, Background } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Required field'),
+        email: Yup.string()
+          .required('Required field')
+          .email('Invalid e-mail, please type again'),
+        password: Yup.string().required('Required field'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <img src={logo} alt="GoBarber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Sign in</h1>
 
           <Input
@@ -30,7 +58,7 @@ const SignIn: React.FC = () => {
           />
           <Button type="submit">Go!</Button>
           <a href="forgot">Forgot your password?</a>
-        </form>
+        </Form>
 
         <a href="register">
           <FiLogIn />
