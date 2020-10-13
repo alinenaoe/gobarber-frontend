9e9ff853a7a93f4322cc7c3b1af9f5eafb481c39
Aline Naoe <alinenaoe@gmail.com>
2020-10-12 21:12:22 -0300

    Set private routes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import React, { useCallback, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { FormHandles } from '@unform/core';
@@ -8,7 +9,7 @@ import * as Yup from 'yup';
 import logo from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimatedContainer } from './styles';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -48,6 +49,7 @@ const SignIn: React.FC = () => {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
           formRef.current?.setErrors(errors);
+          return;
         }
 
         addToast({
@@ -63,33 +65,35 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={logo} alt="GoBarber" />
+        <AnimatedContainer>
+          <img src={logo} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Sign in</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Sign in</h1>
 
-          <Input
-            name="email"
-            icon={FiMail}
-            type="email"
-            placeholder="e-mail"
-            aria-label="E-mail"
-          />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="password"
-            aria-label="Password"
-          />
-          <Button type="submit">Go!</Button>
-          <a href="forgot">Forgot your password?</a>
-        </Form>
+            <Input
+              name="email"
+              icon={FiMail}
+              type="email"
+              placeholder="e-mail"
+              aria-label="E-mail"
+            />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="password"
+              aria-label="Password"
+            />
+            <Button type="submit">Go!</Button>
+            <a href="forgot">Forgot your password?</a>
+          </Form>
 
-        <a href="register">
-          <FiLogIn />
-          Don&apos;t have an account? Sign up!
-        </a>
+          <Link to="/register">
+            <FiLogIn />
+            Don&apos;t have an account? Sign up!
+          </Link>
+        </AnimatedContainer>
       </Content>
       <Background />
     </Container>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,82 +1,115 @@
-/* eslint-disable @typescript-eslint/ban-types */
 import React, { useCallback, useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-
+import api from '../../services/api';
+import { useToast } from '../../hooks/toast';
 import logo from '../../assets/logo.svg';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimatedContainer } from './styles';
 import getValidationErrors from '../../utils/getValidationErrors';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Required field'),
-        email: Yup.string()
-          .required('Required field')
-          .email('Invalid e-mail, please type again'),
-        password: Yup.string().min(
-          6,
-          'Password must contain at least six characters',
-        ),
-      });
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Required field'),
+          email: Yup.string()
+            .required('Required field')
+            .email('Invalid e-mail, please type again'),
+          password: Yup.string().min(
+            6,
+            'Password must contain at least six characters',
+          ),
+        });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err) {
-      const errors = getValidationErrors(err);
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        history.push('/');
+
+        addToast({
+          type: 'success',
+          title: 'Your account has been successfully created!',
+          description: 'Now you can login and enjoy GoBarber',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Registration error',
+          description: 'Please try again',
+        });
+      }
+    },
+    [addToast, history],
+  );
 
   return (
     <Container>
       <Background />
       <Content>
-        <img src={logo} alt="GoBarber" />
+        <AnimatedContainer>
+          <img src={logo} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Sign up</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Sign up</h1>
 
-          <Input
-            name="name"
-            icon={FiUser}
-            type="text"
-            placeholder="Name"
-            aria-label="Name"
-          />
-          <Input
-            name="email"
-            icon={FiMail}
-            type="email"
-            placeholder="E-mail"
-            aria-label="E-mail"
-          />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Password"
-            aria-label="Password"
-          />
-          <Button type="submit">Register</Button>
-        </Form>
+            <Input
+              name="name"
+              icon={FiUser}
+              type="text"
+              placeholder="Name"
+              aria-label="Name"
+            />
+            <Input
+              name="email"
+              icon={FiMail}
+              type="email"
+              placeholder="E-mail"
+              aria-label="E-mail"
+            />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Password"
+              aria-label="Password"
+            />
+            <Button type="submit">Register</Button>
+          </Form>
 
-        <a href="signin">
-          <FiArrowLeft />
-          Back to login page
-        </a>
+          <Link to="/">
+            <FiArrowLeft />
+            Back to login page
+          </Link>
+        </AnimatedContainer>
       </Content>
     </Container>
   );
